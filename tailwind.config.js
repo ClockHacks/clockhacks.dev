d0936e4efbe5afa28e5c0725d9fb8891abb74270
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-08-21 21:30:50 -0400

    init: load colours into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg1: "#1c1c1c",
+        bg2: "#353535",
+        text1: "#eeeeee",
+        text2: "#e0e0e0",
+        accent1: "#10b981",
+        accent2: "#58a370"
+      }
+    },
   },
   plugins: [],
 }
