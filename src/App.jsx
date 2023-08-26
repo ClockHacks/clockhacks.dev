a6fb3aba41fac8b61bf86c05da88d83e271c0e51
Ulises Viña <contacto@ulisesvina.me>
2023-08-26 03:08:21 -0600

    fix: Removed the FAQ section from App.jsx
    Removed the call to the <FAQ /> section on App.jsx, this was used for development

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Navbar from './components/Navbar'
-import FAQ from './sections/FAQ'
 
 function App() {
 
   return (
     <div className='h-screen mb-20'>
       <Navbar />
-      <FAQ />
     </div>
   )
 }
