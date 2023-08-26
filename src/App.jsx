4913b476626025336bfca1453017016e51947580
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-08-26 16:23:47 -0400

    feat: sponsors data and scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Sponsors from './sections/Sponsors/Sponsors'
 
 function App() {
 
   return (
     <div className='h-screen mb-20'>
       <Navbar />
+      <Sponsors />
     </div>
   )
 }
