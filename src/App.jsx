1122116120a26e1d3ea16352c5cf9ade61449bf8
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-08-26 16:25:32 -0400

    fix: remove sponsors from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Navbar from './components/Navbar'
-import Sponsors from './sections/Sponsors/Sponsors'
 
 function App() {
 
   return (
     <div className='h-screen mb-20'>
       <Navbar />
-      <Sponsors />
     </div>
   )
 }
