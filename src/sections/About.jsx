dd91eb6e7000b6b2f646967fd621fd1a6b8c493d
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-08-26 15:11:38 -0400

    fix: correct some spelling mistakes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
         <section className="p-5 md:p-10 flex flex-col justify-center items-center min-h-[50vh] bg-bg2">
             <div className='typography flex flex-col gap-[6rem]'>
                 <div>
-                    <h1 className="text-3xl md:text-4xl font-bold text-center">
+                    <h1 className="text-3xl md:text-4xl font-bold text-center pt-8">
                         By <span className="text-accent1">students</span>, for{" "}
                         <span className="text-accent1">students</span>
                     </h1>
@@ -51,7 +51,7 @@ const About = () => {
                         <h2 className="text-3xl font-bold">
                             <span className="text-accent1">Last year's stats</span>
                         </h2>
-                        <div className="mt-3 text-xl flex justify-around">
+                        <div className="mt-3 text-xl flex flex-col md:flex-row gap-8 justify-center pt-8">
                             <div className='bg-bg1 p-8 rounded-lg text-accent2'>
                                 <span className="font-bold">{stats.participants.toFixed(0)}+</span> <span className="text-accent2">registrations</span>
                             </div>
@@ -69,17 +69,17 @@ const About = () => {
                     <h2 className="text-3xl font-bold">
                         <span className="text-accent1">Project Showcase</span>
                     </h2>
-                    <div className="flex-wrap w-full mt-3">
+                    <div className="max-w-5xl mx-auto mt-3">
                         <a
                             href="https://devpost.com/software/spaced-out-fbv7tz"
                             target="_BLANK"
                             rel="noopener noreferrer"
                         >
-                            <div className="bg-yellow-500 p-3">
+                            <div className="bg-yellow-500 hover:opacity-80 transition-opacity mt-4 rounded-lg p-3">
                                 <span className="font-bold text-xl">Space Out!</span><br />
                                 <span className="text-xs">First Overall</span><br />
                                 <p className="mt-2 text-black">
-                                    Space Out Is a flashcard game designed to help you have fun and study effectively at the same time.
+                                    Studying flash cards has never been more fun!
                                 </p>
                             </div>
                         </a>
@@ -88,7 +88,7 @@ const About = () => {
                             target="_BLANK"
                             rel="noopener noreferrer"
                         >
-                            <div className="bg-gray-400 p-3">
+                            <div className="bg-gray-400 hover:opacity-80 transition-opacity mt-4 rounded-lg p-3">
                                 <span className="font-bold text-xl">Pendulum</span><br />
                                 <span className="text-xs">Second Overall</span><br />
                                 <p className="mt-2 text-black">
@@ -101,11 +101,11 @@ const About = () => {
                             target="_BLANK"
                             rel="noopener noreferrer"
                         >
-                            <div className="bg-yellow-600 p-3">
+                            <div className="bg-yellow-600 hover:opacity-80 transition-opacity mt-4 rounded-lg p-3">
                                 <span className="font-bold text-xl">Optime</span><br />
                                 <span className="text-xs">Third Overall</span><br />
                                 <p className="mt-2 text-black">
-                                    Optime helps you scheduling and planning events.
+                                    For your every smart scheduling need!
                                 </p>
                             </div>
                         </a>
