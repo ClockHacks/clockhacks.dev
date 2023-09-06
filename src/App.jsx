e7082552b5ce3f582ae948dc4c5b4c68d70d46ba
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-09-06 19:35:52 -0400

    uppercase u

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Contact from './sections/Contact'
 
 function App() {
 
   return (
     <div className='h-screen mb-20'>
       <Navbar />
+      <Contact />
     </div>
   )
 }
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
         <div className='flex flex-col items-center justify-center w-full'>
-            <h1 className='md:text-6xl text-4xl text-accent1 font-bold'>Contact us</h1>
+            <h1 className='md:text-6xl text-4xl text-accent1 font-bold'>Contact Us</h1>
             <p className='text-xl mt-5'>Interested in sponsoring? <a href="mailto:business@clockhacks.dev">Reach out to our  <span className="text-accent1 hover:underline">business email.</span></a></p>
             <p className='text-xl mt-2'>Have questions? <a href="mailto:hi@clockhacks.dev">Send us an email <span className="text-accent1 hover:underline">here.</span></a></p>
         </div>
