1b6aaf9eefbf8e0a6efa6a4249d64e2b9c20204e
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-09-06 19:43:02 -0400

    feat: create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className='bg-bg2 p-8 text-text2'>
+      <div className="typography flex justify-between items-center">
+        <div>
+          Copyright © ClockHacks 2023
+        </div>
+        <div className='flex items-center gap-4'>
+          <a href='https://one.clockhacks.dev' className='!text-accent1'>2022 Site</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
