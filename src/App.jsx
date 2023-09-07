2489c889f2ca90a37a4dd86930b9b51b7100a533
Ulises Viña <contacto@ulisesvina.me>
2023-09-06 18:00:26 -0600

    03/faq (#13)
    * feat: FAQ section
    
    Created the FAQ section and modified the file structure to have a FAQ data file
    
    * fix: Removed the FAQ section from App.jsx
    
    Removed the call to the <FAQ /> section on App.jsx, this was used for development
    
    * style: FAQ section
    
    Made the answers aligned to the left as per requested in the pull request feedback and made opening a question in one column don't affect the position on the other
    
    * style: adjust spacing of faq
    
    ---------
    
    Co-authored-by: Jeffrey Zang <zangj4548@wrdsb.ca>
    Co-authored-by: Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import FAQ from './sections/FAQ'
 import Team from './sections/Team'
 import Contact from './sections/Contact'
 import Footer from './components/Footer'
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='h-screen mb-20'>
       <Navbar />
+      <FAQ />
       <Team />
       <Contact />
       <Footer />
---
+++ src/sections/FAQ.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import FAQData from '../data/FAQ.data';
+
+const FAQ = () => {
+    const [activeIndices, setActiveIndices] = useState(Array(FAQData.length).fill(null));
+
+    const toggleAccordion = (columnIndex, questionIndex) => {
+        setActiveIndices(prevIndices => {
+            const newIndices = [...prevIndices];
+            newIndices[columnIndex] = newIndices[columnIndex] === questionIndex ? null : questionIndex;
+            return newIndices;
+        });
+    };
+
+    return (
+        <section className="text-white">
+            <h2 className="text-3xl text-accent1 font-bold mb-8 text-center">Frequently Asked Questions</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-[80%] mx-auto">
+                {FAQData.map((item, columnIndex) => (
+                    <div
+                        key={columnIndex} className='flex flex-col gap-6'
+                    >
+                        <h3 className="text-xl text-accent2">
+                            {item.title}
+                        </h3>
+                        {
+                            item.questions.map((question, questionIndex) => (
+                                <div
+                                    key={questionIndex}
+                                    className="rounded-lg border border-accent2 p-4"
+                                >
+                                    <div
+                                        className="cursor-pointer flex justify-between items-center"
+                                        onClick={() => toggleAccordion(columnIndex, questionIndex)}
+                                    >
+                                        <div className="font-semibold">{question.question}</div>
+                                        <div>{activeIndices[columnIndex] === questionIndex ? '-' : '+'}</div>
+                                    </div>
+                                    <div
+                                        className={`overflow-hidden transition-all duration-300 ${activeIndices[columnIndex] === questionIndex
+                                                ? 'max-h-[1000px] opacity-100'
+                                                : 'max-h-0 opacity-0'
+                                            }`}
+                                        dangerouslySetInnerHTML={{ __html: question.answer }}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default FAQ;
