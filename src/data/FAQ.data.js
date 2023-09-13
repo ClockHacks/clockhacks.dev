441ab538f6fd2cceb5301eeb8348dc349a728018
Ulises Viña <deadbeef@192.168.1.10>
2023-09-12 21:46:29 -0600

    feat: added some questions for the FAQ

--- src/data/FAQ.data.js
+++ src/data/FAQ.data.js
@@ -1,15 +1,28 @@
 const faqData = [
   {
-    title: "Column 1",
+    title: "Event Information",
     questions: [
       {
-        question: "Question 1?",
-        answer: "Answer 1.",
+        question: "What is a Hackathon?",
+        answer: "A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.",
       },
       {
-        question: "Question 2?",
-        answer: "Answer 2 with <a href='#'>link</a>.",
+        question: "How do I become a sponsor?",
+        answer: 'Sponsoring is a great way to grow your business and inspire the youth of the future. If you&apos;re interested in sponsoring, <a href="mailto://business@clockhacks.ca">email our business email.</a>',
       },
+      {
+        question: "Who can participate?",
+        answer: "Students of any age from any part of the world are welcome to participate in ClockHacks. We are aiming for this to be a hackathon targeted toward high school students.",
+      },
+      {
+        question: "Is ClockHacks an in-person hackathon?",
+        answer: "ClockHacks 2023 will be online. Unfortunately, we simply do not have the resources to host ClockHacks in person."
+      },
+      {
+        question: "How much does it cost to attend ClockHacks?",
+        answer: "ClockHacks is completely free to attend. We will not be charging any fees to attend ClockHacks."
+      }
+
     ]
   },
   {
