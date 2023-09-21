62e10514947dd2607f09c9b1c2a8262a879f9657
Jeffrey Zang <jeffrey.zang.07@gmail.com>
2023-09-20 20:44:12 -0400

    update FAQ

--- src/data/FAQ.data.js
+++ src/data/FAQ.data.js
@@ -1,28 +1,49 @@
 const faqData = [
   {
-    title: "Column 1",
+    title: "Event Information",
     questions: [
       {
-        question: "Question 1?",
-        answer: "Answer 1.",
+        question: "What is a Hackathon?",
+        answer: "A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.",
       },
       {
-        question: "Question 2?",
-        answer: "Answer 2 with <a href='#'>link</a>.",
+        question: "How do I become a sponsor?",
+        answer: 'Sponsoring is a great way to grow your business and inspire the youth of the future. If you&apos;re interested in sponsoring, <a href="mailto://business@clockhacks.ca">email our business email.</a>',
       },
+      {
+        question: "Who can participate?",
+        answer: "Students of any age from any part of the world are welcome to participate in ClockHacks. We are aiming for this to be a hackathon targeted toward high school students.",
+      },
+      {
+        question: "Is ClockHacks an in-person hackathon?",
+        answer: "ClockHacks 2023 will be online. Unfortunately, we simply do not have the resources to host ClockHacks in person."
+      },
+      {
+        question: "How much does it cost to attend ClockHacks?",
+        answer: "ClockHacks is completely free to attend. We will not be charging any fees to attend ClockHacks."
+      }
+
     ]
   },
   {
     title: "Column 2",
     questions: [
       {
-        question: "Question 3?",
-        answer: "Answer 3.",
+        question: "You will be able to find a team in the ClockHacks discord before the event starts, or choose to work by yourself.",
+        answer: "You will be able to find a team in the ClockHacks discord before the event starts, or choose to work by yourself.",
       },
       {
-        question: "Question 4?",
-        answer: "Answer 4 with <a href='#'>link</a>.",
+        question: "What if I don't know how to code?",
+        answer: "It's okay! Hackathons aren't just about coding. There is also an aspect of business, design, and presentation. You can also attend workshops and listen to guest speakers talk about the world of tech.",
       },
+      {
+        question: "Can I come if I'm a post-secondary or university student?",
+        answer: "Yes, but you will not be eligible for prizes. In order to keep ClockHacks fair and inclusive, we want to give the best chances to high school students. We still encourage to come, attend workshops, pitch a project, and meet some new people."
+      }, 
+      {
+        question: "What project should I make?",
+        answer: "You can make any type of project you would like! An app, a website, blockchain, hardware, design, or anything that your heart desires. However, since our hackathon is themed around time, we strongly encourage you to base your project around time as well."
+      }
     ]
   },
 ];
